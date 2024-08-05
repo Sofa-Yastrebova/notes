254d10e00141e652c81b4452f97b673b82730320
Sofia Yastrebova <s89055747477@icloud.com>
2024-08-05 17:22:14 +0300

    feat:added style for buttons control

--- dist/main.6a4e4417996f491a2159.js
+++ dist/main.6a4e4417996f491a2159.js
@@ -9005,7 +9005,47 @@ const switchDarkMode = () => {
   document.documentElement.classList.toggle("dark");
 };
 buttonDarkMode.addEventListener("click", switchDarkMode);
+;// CONCATENATED MODULE: ./src/js/modal.js
+// 1.Показывает окно при нажатии на кнопку add note
+// 2.Выбрать версию окна
+// 3.Создать окно и отобразить на странице
+
+const btnAddNote = document.querySelector("#btnAddNote");
+const buttonAddParams = {
+  tagName: "button",
+  classList: ["bg-cyan-600", "px-9"],
+  id: "",
+  text: "Add"
+};
+const buttonEditParams = {
+  tagName: "button",
+  classList: ["bg-cyan-600", "px-9"],
+  id: "",
+  text: "Edit"
+};
+const buttonCancelParams = {
+  tagName: "button",
+  classList: ["bg-[#CD103E]", "px-9"],
+  id: "",
+  text: "Cancel"
+};
+const initialModal = () => {
+  const buttonAdd = creator(buttonAddParams);
+  const buttonEdit = creator(buttonEditParams);
+  const buttonCancel = creator(buttonCancelParams);
+  document.body.append(buttonCancel);
+  document.body.append(buttonAdd);
+  document.body.append(buttonEdit);
+};
+const creator = elementParams => {
+  const element = document.createElement(elementParams.tagName);
+  element.textContent = elementParams.text;
+  element.classList.add(...elementParams.classList);
+  return element;
+};
+btnAddNote.addEventListener("click", initialModal);
 ;// CONCATENATED MODULE: ./src/index-entry.js
+
 
 
 
--- src/js/modal.js
+++ src/js/modal.js
@@ -6,14 +6,56 @@ const btnAddNote = document.querySelector("#btnAddNote");
 
 const buttonAddParams = {
     tagName: "button",
-    classList: ["bg-cyan-600", "px-9"],
+    classList: [
+        "bg-cyan-600",
+        "px-8",
+        "py-1",
+        "rounded-md",
+        "text-white",
+        "min-w-[110px]",
+        "hover:opacity-75",
+    ],
     id: "",
     text: "Add",
 };
 
+const buttonEditParams = {
+    tagName: "button",
+    classList: [
+        "bg-cyan-600",
+        "px-8",
+        "py-1",
+        "rounded-md",
+        "text-white",
+        "min-w-[110px]",
+        "hover:opacity-75",
+    ],
+    id: "",
+    text: "Edit",
+};
+
+const buttonCancelParams = {
+    tagName: "button",
+    classList: [
+        "bg-[#CD103E]",
+        "px-8",
+        "py-1",
+        "rounded-md",
+        "text-white",
+        "min-w-[110px]",
+        "hover:opacity-75",
+    ],
+    id: "",
+    text: "Cancel",
+};
+
 const initialModal = () => {
     const buttonAdd = creator(buttonAddParams);
+    const buttonEdit = creator(buttonEditParams);
+    const buttonCancel = creator(buttonCancelParams);
+    document.body.append(buttonCancel);
     document.body.append(buttonAdd);
+    document.body.append(buttonEdit);
 };
 
 const creator = (elementParams) => {
