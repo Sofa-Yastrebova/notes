19273d605db26ed199aefa40996f4ae0caaeffb1
Sofia Yastrebova <s89055747477@icloud.com>
2024-09-28 19:41:38 +0300

    reactor: decomposed function initialData to getDataFromStorage and setDataToStorage

--- src/index-entry.js
+++ src/index-entry.js
@@ -2,3 +2,4 @@ import "./index.html";
 import "./main.css";
 import "./js/dark-mode.js";
 import "./js/modal/modal.js";
+import "./js/utilities/data-handler.js";
--- src/js/utilities/data-handler.js
+++ src/js/utilities/data-handler.js
@@ -1,25 +1,39 @@
-// const notes = {
-//     regulary: [],
-//     favorites: [],
-// }
-
 // 1. Создать функцию для инициализации данных в локальном хранилище
 // 2. Дополнить объект заметки нужными значениями(text, id, status)
+// const notes =
 
-// const InitialData = () => {
-
-// }
-
-export const setDataToStorage = (form) => {
-    const dataString = JSON.stringify(handlerData(form));
+const setDataToStorage = (notes) => {
+    const dataString = JSON.stringify(notes);
     localStorage.setItem("notes", dataString);
 };
 
-const handlerData = (form) => {
-    const formData = new FormData(form);
-    const newNote = {
-        title: formData.get("input-title"),
-    };
-
-    return newNote;
+const getDataFromStorage = () => {
+    const dataFromLocal = localStorage.getItem("notes");
+    const dataParse = JSON.parse(dataFromLocal);
+    return dataParse;
 };
+
+const InitialData = () => {
+    const isDataFromStorage = getDataFromStorage();
+
+    if (isDataFromStorage) {
+        console.log(isDataFromStorage);
+    } else {
+        const notes = {
+            regulary: [],
+            favorites: [],
+        };
+        setDataToStorage(notes);
+    }
+};
+
+InitialData();
+
+// const handlerData = (form) => {
+//     const formData = new FormData(form);
+//     const newNote = {
+//         title: formData.get("input-title"),
+//     };
+
+//     return newNote;
+// };
